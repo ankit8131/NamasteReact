af2753e5728ea065fa011961983dbde0ecb2671a
Ankit Kumar <ankit@Ankits-MacBook-Air.local>
2023-09-05 08:17:50 +0530

    Episode 1 React

---
+++ App.js
@@ -0,0 +1,11 @@
+const heading = React.createElement("h1", {
+    id:"heading"
+}, "Hello From React");
+
+const parent= React.createElement("div",{id:"parent"},
+    React.createElement("div",{id:"child"},
+    [React.createElement("h1",{},"I am an h1 tag"),React.createElement("h2",{},"I am an h2 tag")])
+)
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);
